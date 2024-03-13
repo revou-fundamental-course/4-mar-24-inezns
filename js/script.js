36a6a39272c88abd7e6f84ae20c334f6cdb53c64
inezns <162710434+inezns@users.noreply.github.com>
2024-03-13 21:09:38 +0700

    13marchlagi

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-let json = "{bad json}";
-try{
-    let user = JSON.parse(json);
-}catch(err){
-    console.log("error name:", err.name);
-    console.log("error message:", err.message);
-}
-let age = document.getElementById("age").value;
-let weight = document.getElementById("weight").value;
-let height = document.getElementById("height").value;
+// let json = "{bad json}";
+// try{
+//     let user = JSON.parse(json);
+// }catch(err){
+//     console.log("error name:", err.name);
+//     console.log("error message:", err.message);
+// }
+let age = document.getElementById("age");
+let weight = document.getElementById("weight");
+let height = document.getElementById("height");
 let HitungBMI_button = document.getElementById("HitungBMI_button")
 let BMIResult = document.getElementById("BMIResult");
 
-HitungBMI_button.addEventListener("click",function()){
-    if(isNaN(weight,height)||weight==''||height==''){
+HitungBMI_button.addEventListener("click",function(){
+    // console.log(weight.value=='');
+    // console.log(height.value=='');
+    // console.log(isNaN(weight.value), isNaN(height.value), weight.value, height.value);
+    if(isNaN(weight.value,height.value)||weight.value==''||height.value==''){
+        console.log('masuk true');
         weight.value = 0;
         height.value = 0;
         BMIResult.innerHTML = 0;
     }else{
-        BMIResult.innerHTML = weight / ((height*height/10000)) ;
+        BMIResult.innerHTML = weight.value / ((height.value*height.value/10000)) ;
     }
-}
+})
 
 
 
